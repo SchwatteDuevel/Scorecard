4b0550db00ff24f54c8f71691c30724bd3ad527f
SchwatteDuevel <102264881+SchwatteDuevel@users.noreply.github.com>
2022-06-15 16:02:39 +0200

    Highlight Leader

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const resultHome = document.getElementById("home");
 const resultGuest = document.getElementById("guest");
+const highlightHome = document.getElementsByClassName("highlight-home");
+const highlightGuest = document.getElementsByClassName("highlight-guest");
 let count = 0;
 let countHome = 0;
 let countGuest = 0;
@@ -7,20 +9,38 @@ let countGuest = 0;
 function addHome(points) {
     countHome += points;
     resultHome.textContent = countHome; 
+    highlightLeader();
+    return countHome;
 }
 
 function addGuest(points) {
     countGuest += points;
-    resultGuest.textContent = countGuest; 
+    resultGuest.textContent = countGuest;
+    highlightLeader();
+    return countGuest;
 }
+
+function highlightLeader() {
+  if(countHome > countGuest) {
+    highlightHome[0].style.backgroundColor = "#00ff00";
+    highlightGuest[0].style.backgroundColor = "#000";
+    
+  } else if (countHome < countGuest) {
+    highlightGuest[0].style.backgroundColor = "#00ff00";
+    highlightHome[0].style.backgroundColor = "#000";
+  }
+    else {
+    highlightHome[0].style.backgroundColor = "#000";
+    highlightGuest[0].style.backgroundColor = "#000";
+    }
+}
+
 
 function reset() {
     countHome = 0;
     countGuest = 0;
     resultHome.textContent = 0;
     resultGuest.textContent = 0;
-}
-
-function highLightLeader() {
-    
+    highlightHome[0].style.backgroundColor = "#000";
+    highlightGuest[0].style.backgroundColor = "#000";
 }
