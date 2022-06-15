449d6c62875551d5bdec466d5e18fd1c12265409
SchwatteDuevel <102264881+SchwatteDuevel@users.noreply.github.com>
2022-06-15 17:50:27 +0200

    Code refactoring

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const resultHome = document.getElementById("home");
 const resultGuest = document.getElementById("guest");
-const highlightHome = document.getElementsByClassName("highlight-home");
-const highlightGuest = document.getElementsByClassName("highlight-guest");
 let count = 0;
 let countHome = 0;
 let countGuest = 0;
@@ -22,25 +20,24 @@ function addGuest(points) {
 
 function highlightLeader() {
   if(countHome > countGuest) {
-    highlightHome[0].style.backgroundColor = "#00ff00";
-    highlightGuest[0].style.backgroundColor = "#000";
+    resultHome.classList.add("highlight");
+    resultGuest.classList.remove("highlight");
     
   } else if (countHome < countGuest) {
-    highlightGuest[0].style.backgroundColor = "#00ff00";
-    highlightHome[0].style.backgroundColor = "#000";
+    resultHome.classList.remove("highlight");
+    resultGuest.classList.add("highlight");
   }
-    else {
-    highlightHome[0].style.backgroundColor = "#000";
-    highlightGuest[0].style.backgroundColor = "#000";
-    }
+  else {
+    resultHome.classList.remove("highlight");
+    resultGuest.classList.remove("highlight");
+  }
 }
-
 
 function reset() {
     countHome = 0;
     countGuest = 0;
     resultHome.textContent = 0;
     resultGuest.textContent = 0;
-    highlightHome[0].style.backgroundColor = "#000";
-    highlightGuest[0].style.backgroundColor = "#000";
+    resultGuest.classList.remove("highlight");
+    resultHome.classList.remove("highlight");
 }
